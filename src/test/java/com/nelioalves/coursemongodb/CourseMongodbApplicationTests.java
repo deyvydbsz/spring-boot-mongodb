1552d9bcb11c3b5e210650d5dc8a294a7a452bac
Deyvyd Zaccariotto <deyvydbsz@live.jp>
2022-03-08 22:24:03 -0300

    Project created

---
+++ src/test/java/com/nelioalves/coursemongodb/CourseMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelioalves.coursemongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
