6da3f53ae1bd8e9f72574fb194937c493e0cff50
Deyvyd Zaccariotto <deyvydbsz@live.jp>
2022-03-18 03:11:49 -0300

    Database instantiation operation

---
+++ src/main/java/com/nelioalves/coursemongodb/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.nelioalves.coursemongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.nelioalves.coursemongodb.domain.User;
+import com.nelioalves.coursemongodb.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}
